a8de93fd07918c9b7106c2d3580f7c8c2cb5b609
akeeton <andrewrkeeton@gmail.com>
2024-10-20 13:47:44 -0400

    Add pubsub.PublishJSON()

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,24 +6,45 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	const connectionString = "amqp://guest:guest@localhost:5672/"
 
-	connection, err := amqp.Dial(connectionString)
+	conn, err := amqp.Dial(connectionString)
 	if err != nil {
 		log.Fatal("Error connecting to RabbitMQ:", err)
 	}
-	defer connection.Close()
+	defer conn.Close()
 
 	fmt.Println("Connected to RabbitMQ")
+
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatal("Error creating RabbitMQ channel:", err)
+	}
+
+	err = pubsub.PublishJSON(
+		publishCh,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{
+			IsPaused: true,
+		},
+	)
+	if err != nil {
+		log.Println("Error publishing JSON to channel:", err)
+	}
+
+	fmt.Println("Pause message sent!")
 
 	// Wait for Ctrl+C
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, os.Interrupt)
 	<-signalCh
 	fmt.Println("Received Ctrl+C, shutting down connection to RabbitMQ")
-	connection.Close()
+	conn.Close()
 }
